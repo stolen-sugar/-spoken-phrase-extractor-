4a953a309cf83552b943ff7965165f74ced3eb61
Ronald Walker <ron22@hey.com>
2022-03-27 08:50:21 -0700

    init

---
+++ spoken.sh
@@ -0,0 +1,26 @@
+# run this script from ~/.talon/user
+counter=0
+sleep 1
+iterate_over_all_files()
+{
+   for FILE in ../users/*;
+   do
+   filename=$(basename "$FILE")
+   mv $FILE .
+   sleep 2
+   mv talon_commands.json talon_commands$counter.json
+   sleep 1;
+   mv talon_commands$counter.json ../commands
+   mv $filename ../users/
+   sleep 9;
+   counter=$((counter+1))
+   done
+}
+ 
+iterate_over_all_files
+ 
+ 
+ 
+ 
+ 
+# restart_talon
